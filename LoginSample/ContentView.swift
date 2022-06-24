e36b5728822bdbf225da03e397575a081a5f982a
Christian Ray Leovido <christian.leovido@gmail.com>
2022-06-24 15:18:26 +0100

    Initial Commit

---
+++ LoginSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginSample
+//
+//  Created by Christian Ray Leovido on 24/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginSample/LoginSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSampleApp.swift
+//  LoginSample
+//
+//  Created by Christian Ray Leovido on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
